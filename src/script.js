d7902e3f7094d9b6d451bc6b1775c7a43d6911aa
Ulrike <ulrike.hacker@quantilope.com>
2020-11-09 08:50:52 +0100

    add first button functionality and overlay

---
+++ src/script.js
@@ -0,0 +1,20 @@
+function init(){
+
+
+
+var button = document.getElementById("add-mo-session");
+
+
+button.addEventListener('click', function(){
+    //general test for querie functioning DELETE ME
+    console.log("I found you");
+
+    //button should not be displayed in overlay mode, just plain site below the overlay
+    button.parentElement.removeChild(button);
+
+    //create overlay on button click
+    document.getElementById("overlay").style.display = "block";
+
+    
+    });
+}

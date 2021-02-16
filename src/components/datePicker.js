a5b4869a7c79fa27cee9cb478dc5a32c1571219a
Ludwig Walther <ludwig.walther@quantilope.com>
2021-02-16 20:43:04 +0100

    make input display from and to dates

--- src/components/datePicker.js
+++ src/components/datePicker.js
@@ -1,44 +1,56 @@
+/* eslint-disable import/prefer-default-export */
 import { Datepicker } from 'vanillajs-datepicker';
 import de from 'vanillajs-datepicker/js/i18n/locales/de';
+
 Object.assign(Datepicker.locales, de);
 
-export const renderDatepicker = (removeWeekFromDOMAndRenderSelectedWeek) => {
-  const inputElement = document.querySelector('input[name="date-picker"]');
-
-  // set this week's monday date as input value
-  const curr = new Date();
-  const dateOfThisMonday = new Date(
-    curr.setDate(curr.getDate() - curr.getDay() + 1)
-  );
+const getMondayToSundayString = (curr) => {
+  const thisMonday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 1));
+  const thisSunday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 7));
   const options = {
     year: 'numeric',
     month: '2-digit',
-    day: '2-digit'
+    day: '2-digit',
   };
-  const formattedDateOfThisMonday = dateOfThisMonday.toLocaleDateString(
-    'de-DE',
-    options
-  );
-  inputElement.value = formattedDateOfThisMonday;
+  const formattedThisMonday = thisMonday.toLocaleDateString('de-DE', options);
+  const formattedThisSunday = thisSunday.toLocaleDateString('de-DE', options);
+  return `${formattedThisMonday} - ${formattedThisSunday}`;
+};
 
+export const renderDatepicker = (removeWeekFromDOMAndRenderSelectedWeek) => {
+  const inputElement = document.querySelector('input[name="date-picker"]');
   const datepicker = new Datepicker(inputElement, {
     daysOfWeekDisabled: [0, 2, 3, 4, 5, 6],
     daysOfWeekHighlighted: [1],
     language: 'de',
-    weekStart: 1
+    weekStart: 1,
+    autohide: true,
+    format: {
+      toValue(date) {
+        return date;
+      },
+      toDisplay(date) {
+        return getMondayToSundayString(date);
+      },
+    },
   });
-  renderWeekWithSelectedDate();
-  inputElement.addEventListener('changeDate', renderWeekWithSelectedDate);
+
+  const curr = new Date();
+  inputElement.value = getMondayToSundayString(curr);
 
   function renderWeekWithSelectedDate() {
     const selectedDates = {
       selectedDate: datepicker.getDate('yyyymmdd'),
-      dateOfThisMonday: datepicker.getDate()
+      dateOfThisMonday: datepicker.getDate(),
     };
+    inputElement.style.width = inputElement.value.length - 4 + 'ch';
     removeWeekFromDOMAndRenderSelectedWeek(selectedDates);
   }
+
+  renderWeekWithSelectedDate();
+  inputElement.addEventListener('changeDate', renderWeekWithSelectedDate);
 };
 
-//https://mymth.github.io/vanillajs-datepicker/#/
-//https://raw.githack.com/mymth/vanillajs-datepicker/v1.1.2/demo/index.html
-//https://stackoverflow.com/questions/5210376/how-to-get-first-and-last-day-of-the-current-week-in-javascript
+// https://mymth.github.io/vanillajs-datepicker/#/
+// https://raw.githack.com/mymth/vanillajs-datepicker/v1.1.2/demo/index.html
+// https://stackoverflow.com/questions/5210376/how-to-get-first-and-last-day-of-the-current-week-in-javascript
